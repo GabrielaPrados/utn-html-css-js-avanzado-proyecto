6932c12a5fb37de0febd5781d76e2c52920707fc
Gabriela Prados <gabrielavprados@gmail.com>
2023-01-03 14:48:49 -0300

    Proyecto primer etapa con 3 paginas y responsive

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+
+
+let video = document.querySelector("#video")
+
+
+document.addEventListener("click", (e) => {
+    const et = e.target
+    if (et.matches("#play")) {
+        video.play()
+        let videoDuration = document.querySelector("#videoControls p")
+        const duration = video.duration / 60 
+        const minutes = Math.trunc(duration)
+        const minutesOff = duration - minutes
+        const seconds = Math.trunc(minutesOff * 60) 
+        videoDuration.innerText=`Duración total del video: ${minutes}:${seconds} `
+    }
+    if (et.matches("#stop")){
+        video.pause()
+    }
+})
